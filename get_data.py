41a55ced7be13c4ce4e10c7642eeaf6990fc7a02
abhishekarjs <abhishekvaidhav@gmail.com>
2024-08-19 00:42:02 +0530

    Adding some extra codes

--- get_data.py
+++ get_data.py
@@ -2,7 +2,10 @@ import os
 import json
 import numpy
 from dotenv import load_dotenv
+load_dotenv()
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 import certifi
+ca = certifi.where()
 import pandas as pd
 import numpy as np
 import pymongo
