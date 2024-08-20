743df5d495b49a6d6c1b56c7b789d11d1ea3d377
abhishekarjs <abhishekvaidhav@gmail.com>
2024-08-20 17:28:59 +0530

    Training Master File Updated

---
+++ start_training.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from networksecurity.exceptions.exception import NetworkSecurityException
+from networksecurity.loggers.logger import logging
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
+
+def start_training():
+    try:
+        pass
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+if __name__=="__main__":
+    start_training()
